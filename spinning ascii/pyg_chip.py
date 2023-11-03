b2a92fae91a953642a1d92ef8b5838ef3034ea2a
omar-elsherbiny <137009632+omar-elsherbiny@users.noreply.github.com>
2023-11-04 01:13:43 +0200

    editted chip colors

--- spinning ascii/pyg_chip.py
+++ spinning ascii/pyg_chip.py
@@ -3,6 +3,7 @@ import pygame as pyg
 from sys import exit as syexit
 from MatrixObj import Matrix
 from math import sin, cos, radians, sqrt
+from random import randint
 
 pyg.init()
 
@@ -27,11 +28,14 @@ def main():
     pointz=Matrix('3x1', [[0],[0],[1]])*S
 
     pnts=[]
+    cpnts=[]
     rX=Matrix('3x3',[[1,0,0],[0,cos(radians(30)),-sin(radians(30))],[0,sin(radians(30)),cos(radians(30))]])
     for phi in range(-100,100,10):
         for theta in range(round(-70*cos(phi/100)),round(70*cos(phi/100)),10):
             if phi<80*cos(theta/100) and phi>-80*cos(theta/100):
                 pnts.append(rX@Matrix('3x1',[[theta],[phi+20],[(theta**2-phi**2)/300+50]]))
+                cr=randint(145,230)
+                cpnts.append((cr,205*cr/230,150*cr/230))
 
     #MAIN LOOP
     run = True
@@ -57,17 +61,17 @@ def main():
         rz=rot@pointz
 
         rot_pnts=[rot@pnt for pnt in pnts]
-        rot_pnts.sort(key=lambda x: x.matrix[2][0])
+        #rot_pnts.sort(key=lambda x: x.matrix[2][0])
 
         SCREEN.fill(BG_COLOR)
         #pyg.draw.line(SCREEN,(255, 10, 50),(250,250),(rx.matrix[0][0]+250,-rx.matrix[1][0]+250),3)
         #pyg.draw.line(SCREEN,(50, 255, 10),(250,250),(ry.matrix[0][0]+250,-ry.matrix[1][0]+250),3)
         #pyg.draw.line(SCREEN,(10, 50, 255),(250,250),(rz.matrix[0][0]+250,-rz.matrix[1][0]+250),3)
         
-        for pnt in rot_pnts:
+        for i,pnt in enumerate(rot_pnts):
             dist=dist_3d(pnt,Matrix('3x1',[[0],[50],[200]]))
             c=(220-dist/2)%255
-            pyg.draw.circle(SCREEN,(c,c,c),(int(pnt.matrix[0][0]+250),int(-pnt.matrix[1][0]+250+10*sin(Ay/40))),5)
+            pyg.draw.circle(SCREEN,((c+2*cpnts[i][0])/3,(c+2*cpnts[i][1])/3,(c+2*cpnts[i][2])/3),(int(pnt.matrix[0][0]+250),int(-pnt.matrix[1][0]+250+10*sin(Ay/40))),8)
             #draw_text(lightness[round(c*7/85)],int(pnt.matrix[0][0]+250),int(-pnt.matrix[1][0]+250))
 
         clock.tick(60)
