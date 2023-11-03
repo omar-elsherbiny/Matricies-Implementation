29e6e649a2fb24de22c320b6d9b933973d320a5a
omar-elsherbiny <137009632+omar-elsherbiny@users.noreply.github.com>
2023-11-04 00:47:46 +0200

    v1.0

---
+++ spinning ascii/pyg_ascii_donut.py
@@ -0,0 +1,86 @@
+#Imports
+import pygame as pyg
+from sys import exit as syexit
+from MatrixObj import Matrix
+from math import sin, cos, radians, sqrt
+
+pyg.init()
+
+#Globals
+SCREEN_WIDTH = 500
+SCREEN_HEIGHT = 500
+BG_COLOR=(20,20,20)
+SCREEN = pyg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+FONT = pyg.font.Font("freesansbold.ttf", 20)
+
+def dist_3d(pnt1,pnt2):
+    return sqrt((pnt1.matrix[0][0]-pnt2.matrix[0][0])**2+(pnt1.matrix[1][0]-pnt2.matrix[1][0])**2+(pnt1.matrix[2][0]-pnt2.matrix[2][0])**2)
+
+def draw_text(letter,x,y,c):
+    text=FONT.render(letter,True,(c,c,c))
+    SCREEN.blit(text,(x,y))
+
+#Main
+def main():
+    clock = pyg.time.Clock()
+
+    lightness=" .,'`-+^:;(|/*#MW&8%B@$"
+
+    R=70
+    Rh=150
+    S=100
+    Ax,Ay,Az=30, 0, 0
+    pointx=Matrix('3x1', [[1],[0],[0]])*S
+    pointy=Matrix('3x1', [[0],[1],[0]])*S
+    pointz=Matrix('3x1', [[0],[0],[1]])*S
+
+    pnts=[]
+    for phi in range(0,360,10):
+        #rY=Matrix('3x3',[[cos(radians(phi)),0,-sin(radians(phi))],[0,1,0],[sin(radians(phi)),0,cos(radians(phi))]])
+        rZ=Matrix('3x3',[[cos(radians(phi)),-sin(radians(phi)),0],[sin(radians(phi)),cos(radians(phi)),0],[0,0,1]])
+        for theta in range(0,360,30):
+            pnts.append(rZ@Matrix('3x1',[[0],[R*sin(radians(theta))+Rh],[R*cos(radians(theta))]]))
+
+    #MAIN LOOP
+    run = True
+    while run:
+        for event in pyg.event.get():
+            if event.type == pyg.QUIT:
+                pyg.quit()
+                syexit()
+            elif event.type == pyg.MOUSEBUTTONDOWN:
+                if event.button == 1:            
+                    pass
+        
+        Ax += 1
+        Ay += 1
+        Az += 1
+        rotX=Matrix('3x3',[[1,0,0],[0,cos(radians(Ax)),-sin(radians(Ax))],[0,sin(radians(Ax)),cos(radians(Ax))]])
+        rotY=Matrix('3x3',[[cos(radians(Ay)),0,-sin(radians(Ay))],[0,1,0],[sin(radians(Ay)),0,cos(radians(Ay))]])
+        rotZ=Matrix('3x3',[[cos(radians(Az)),-sin(radians(Az)),0],[sin(radians(Az)),cos(radians(Az)),0],[0,0,1]])
+        rot=rotX@rotY@rotZ
+
+        rx=rot@pointx
+        ry=rot@pointy
+        rz=rot@pointz
+
+        rot_pnts=[rot@pnt for pnt in pnts]
+        rot_pnts.sort(key=lambda x: x.matrix[2][0])
+
+        SCREEN.fill(BG_COLOR)
+        #pyg.draw.line(SCREEN,(255, 10, 50),(250,250),(rx.matrix[0][0]+250,-rx.matrix[1][0]+250),3)
+        #pyg.draw.line(SCREEN,(50, 255, 10),(250,250),(ry.matrix[0][0]+250,-ry.matrix[1][0]+250),3)
+        #pyg.draw.line(SCREEN,(10, 50, 255),(250,250),(rz.matrix[0][0]+250,-rz.matrix[1][0]+250),3)
+        
+        for pnt in rot_pnts:
+            dist=dist_3d(pnt,Matrix('3x1',[[0],[50],[200]]))
+            c=71000/(dist+200)-100
+            #pyg.draw.circle(SCREEN,(c,c,c),(int(pnt.matrix[0][0]+250),int(-pnt.matrix[1][0]+250)),5)
+            draw_text(lightness[round(c*23/255)],int(pnt.matrix[0][0]+250),int(-pnt.matrix[1][0]+250),c/4+375/2)
+
+        clock.tick(60)
+        pyg.display.set_caption(f'Rendering--{int(clock.get_fps())}')
+        pyg.display.update()
+
+if __name__=='__main__':
+    main()
--- spinning ascii/pyg_test copy.py
+++ spinning ascii/pyg_test copy.py
@@ -20,20 +20,18 @@ def dist_3d(pnt1,pnt2):
 def main():
     clock = pyg.time.Clock()
 
-    R=70
-    Rh=150
     S=100
-    Ax,Ay,Az=30, 0, 0
+    Ax,Ay,Az=15, 0, 0
     pointx=Matrix('3x1', [[1],[0],[0]])*S
     pointy=Matrix('3x1', [[0],[1],[0]])*S
     pointz=Matrix('3x1', [[0],[0],[1]])*S
 
     pnts=[]
-    for phi in range(0,360,10):
-        #rY=Matrix('3x3',[[cos(radians(phi)),0,-sin(radians(phi))],[0,1,0],[sin(radians(phi)),0,cos(radians(phi))]])
-        rZ=Matrix('3x3',[[cos(radians(phi)),-sin(radians(phi)),0],[sin(radians(phi)),cos(radians(phi)),0],[0,0,1]])
-        for theta in range(0,360,10):
-            pnts.append(rZ@Matrix('3x1',[[0],[R*sin(radians(theta))+Rh],[R*cos(radians(theta))]]))
+    rX=Matrix('3x3',[[1,0,0],[0,cos(radians(30)),-sin(radians(30))],[0,sin(radians(30)),cos(radians(30))]])
+    for phi in range(-100,100,10):
+        for theta in range(round(-70*cos(phi/100)),round(70*cos(phi/100)),10):
+            if phi<80*cos(theta/100) and phi>-80*cos(theta/100):
+                pnts.append(rX@Matrix('3x1',[[theta],[phi+20],[(theta**2-phi**2)/300+50]]))
 
     #MAIN LOOP
     run = True
@@ -46,9 +44,9 @@ def main():
                 if event.button == 1:            
                     pass
         
-        Ax += 1
-        Ay += 1
-        Az += 1
+        Ax += 0
+        Ay += 2
+        Az += 0
         rotX=Matrix('3x3',[[1,0,0],[0,cos(radians(Ax)),-sin(radians(Ax))],[0,sin(radians(Ax)),cos(radians(Ax))]])
         rotY=Matrix('3x3',[[cos(radians(Ay)),0,-sin(radians(Ay))],[0,1,0],[sin(radians(Ay)),0,cos(radians(Ay))]])
         rotZ=Matrix('3x3',[[cos(radians(Az)),-sin(radians(Az)),0],[sin(radians(Az)),cos(radians(Az)),0],[0,0,1]])
@@ -62,14 +60,15 @@ def main():
         rot_pnts.sort(key=lambda x: x.matrix[2][0])
 
         SCREEN.fill(BG_COLOR)
-        pyg.draw.line(SCREEN,(255, 10, 50),(250,250),(rx.matrix[0][0]+250,-rx.matrix[1][0]+250),3)
-        pyg.draw.line(SCREEN,(50, 255, 10),(250,250),(ry.matrix[0][0]+250,-ry.matrix[1][0]+250),3)
-        pyg.draw.line(SCREEN,(10, 50, 255),(250,250),(rz.matrix[0][0]+250,-rz.matrix[1][0]+250),3)
+        #pyg.draw.line(SCREEN,(255, 10, 50),(250,250),(rx.matrix[0][0]+250,-rx.matrix[1][0]+250),3)
+        #pyg.draw.line(SCREEN,(50, 255, 10),(250,250),(ry.matrix[0][0]+250,-ry.matrix[1][0]+250),3)
+        #pyg.draw.line(SCREEN,(10, 50, 255),(250,250),(rz.matrix[0][0]+250,-rz.matrix[1][0]+250),3)
         
         for pnt in rot_pnts:
             dist=dist_3d(pnt,Matrix('3x1',[[0],[50],[200]]))
             c=(220-dist/2)%255
-            pyg.draw.circle(SCREEN,(c,c,c),(int(pnt.matrix[0][0]+250),int(-pnt.matrix[1][0]+250)),5)
+            pyg.draw.circle(SCREEN,(c,c,c),(int(pnt.matrix[0][0]+250),int(-pnt.matrix[1][0]+250+10*sin(Ay/40))),5)
+            #draw_text(lightness[round(c*7/85)],int(pnt.matrix[0][0]+250),int(-pnt.matrix[1][0]+250))
 
         clock.tick(60)
         pyg.display.set_caption(f'Rendering--{int(clock.get_fps())}')
@@ -77,6 +76,3 @@ def main():
 
 if __name__=='__main__':
     main()
-# init angles
-# put rotation
-# alter rotation matrix
